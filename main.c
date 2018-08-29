8cb9ccc1790e9e91b0190579e9387a74ebdc5f16
Thiago Classen <tgclassen.dev@gmail.com>
2018-08-29 17:21:58 -0300

    separavel done

--- main.c
+++ main.c
@@ -9,7 +9,9 @@
 
 
 void blurIngenuo(Imagem *original);
-void fillBorders(Imagem *original);
+void fillBorders(Imagem *imagem);
+void separavel(Imagem *imagem);
+void integral(Imagem *imagem);
 
 int main() {
 
@@ -26,6 +28,7 @@ int main() {
 
     blurIngenuo(imagem);
 
+    separavel(imagem);
 
     return 0;
 }
@@ -33,7 +36,7 @@ int main() {
 
 void blurIngenuo(Imagem *original){
     int border_x = JANELA_X/2;
-    int border_y = JANELA_Y/2;
+    int border_y = JANELA_Y/2;    
     float total = 0.0f;
     
     Imagem *buffer;
@@ -44,7 +47,7 @@ void blurIngenuo(Imagem *original){
         // linha
         for(int y = border_y;y<original->altura-border_y;y++){
             // coluna
-            for(int x = border_x;x<original->altura-border_x;x++){
+            for(int x = border_x;x<original->largura-border_x;x++){
                 // janela
                 total = 0.0f;
 
@@ -59,10 +62,100 @@ void blurIngenuo(Imagem *original){
         }
     }
 
+    fillBorders(buffer);
+
     salvaImagem(buffer,"blurIngenuo");
 }
 
-void fillBorders(Imagem *original){
-    
+void fillBorders(Imagem *imagem){
 
+    int border_x = JANELA_X/2;
+    int border_y = JANELA_Y/2;
+    
+    for(int c = 0; c < 3;c++){
+        //borda esquerda
+        for(int x = 0; x < border_x; x++){
+            for(int y = 0; y < imagem->altura; y++){
+                imagem->dados[c][y][x] = 0.0f;
+            }
+        }
+
+        // borda direita
+        for(int x = imagem->largura - border_x; x < imagem->largura; x++){
+            for(int y = 0; y < imagem->altura; y++){
+                imagem->dados[c][y][x] = 0.0f;
+            }
+        }
+
+        // borda cima
+        for(int x = border_x; x < imagem->largura - border_x; x++){
+            for(int y = 0; y < border_y; y++){
+                imagem->dados[c][y][x] = 0.0f;
+            }
+        }
+
+        // borda baixo
+        for(int x = border_x; x < imagem->largura - border_x; x++){
+            for(int y = imagem->altura - border_y; y < imagem->altura; y++){
+                imagem->dados[c][y][x] = 0.0f;
+            }
+        }
+    }
+
+}
+
+void separavel(Imagem *imagem){
+    
+    int border_x = JANELA_X/2;
+    int border_y = JANELA_Y/2;
+    float total;
+    
+    Imagem *buffer, *final;
+
+    copiaConteudo(imagem, buffer);
+
+    // filtro horizontal
+
+    // canal
+    for(int c = 0;c<3;c++){
+        // linha
+        for(int y = 0;y<imagem->altura;y++){
+            // coluna
+            for(int x = border_x;x<imagem->largura-border_x;x++){
+                // janela
+                total = 0.0f;
+
+                for(int j_x = x-border_x; j_x<x+border_x;j_x++){
+                    total += imagem->dados[c][y][j_x];
+                }   
+
+                buffer->dados[c][y][x] = total/(JANELA_X);
+            }      
+        }
+    }
+
+    //salvaImagem(buffer,"separavelHorizontal");
+
+    // filtro vertical
+    // canal
+    for(int c = 0;c<3;c++){
+        // linha
+        for(int y = border_y; y < imagem->altura-border_y; y++){
+            // coluna
+            for(int x = 0; x < imagem->largura; x++){
+                // janela
+                total = 0.0f;
+
+                for(int j_y = y-border_y; j_y<y+border_y;j_y++){
+                    total += imagem->dados[c][j_y][x];
+                }  
+
+                buffer->dados[c][y][x] = total/(JANELA_Y);
+            }      
+        }
+    }
+
+    fillBorders(buffer);
+
+    salvaImagem(buffer,"separavel");
 }
